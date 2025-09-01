fbbc2dab79583bd6fac501b3d2db6bac962cca8d
QuackieMackie <dev.quackiemackie@gmail.com>
2025-09-01 20:52:20 +0100

    Added an error message if addon errors exist. - This was implemented the same way as the Xenforo server error message. - Bumped version to v1.0.1 Alpha

---
+++ _output/extension_hint.php
@@ -0,0 +1,14 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ */
+
+namespace Sylphian\Library\XF\Admin\Controller
+{
+	class XFCP_IndexController extends \XF\Admin\Controller\IndexController {}
+}
